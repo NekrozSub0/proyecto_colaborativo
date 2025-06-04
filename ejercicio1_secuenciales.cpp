95306ebd1bd600364d17443610c6e18ad9420713
Alexander <alex-c-r@hotmail.es>
2025-06-03 23:51:48 -0500

    Agregado de ejercicio 1 de secuenciales

---
+++ ejercicio1_secuenciales.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	float a,b,c;
+	cout<<"Ingrese la altura del rectangulo: ";
+	cin>>a;
+	cout<<"Ingrese la base del rectangulo: ";
+	cin>>b;
+	c = 2*a + 2*b;
+	cout<<"el perimetro del rectangulo es: "<<c<<endl;
+	return 0;
+}
+
