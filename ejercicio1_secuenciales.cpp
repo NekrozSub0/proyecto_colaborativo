043c0c8c6050f2f4dc82104e0e7cb68e9c548744
josue <josue.mlpro.2014@gmail.com>
2025-06-04 22:40:30 -0500

    agregue el titulo del programa

--- ejercicio1_secuenciales.cpp
+++ ejercicio1_secuenciales.cpp
@@ -1,8 +1,11 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main(){
 	float a,b,c;
+	cout<<"perimetro de un rectangulo"<<endl;
+	
+	
 	cout<<"Ingrese la altura del rectangulo: ";
 	cin>>a;
 	cout<<"Ingrese la base del rectangulo: ";
