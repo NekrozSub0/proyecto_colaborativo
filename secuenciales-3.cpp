42853c526d4f41fb1156193fb235260574ebc5ba
josue <josue.mlpro.2014@gmail.com>
2025-06-07 15:03:44 -0500

    agregue un codigo de secuenciales

---
+++ secuenciales-3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int perimetro, apotema, area;
+	
+	cin>> perimetro;
+	cin>> apotema;
+	
+	area= (perimetro*apotema)/2;
+	
+	cout<< area;
+	
+	return 0;
+}
