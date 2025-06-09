b76a663d54caa6e973a4579fd3c99daf6c34fd02
Alexander <alex-c-r@hotmail.es>
2025-06-09 12:50:23 -0500

    Agregado de fases en el codigo para ser mas entendible

--- secuenciales-3.cpp
+++ secuenciales-3.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
+	//Declaracion de variables
 	int perimetro, apotema, area;
-	
+	//Entrada
 	cin>> perimetro;
 	cin>> apotema;
-	
+	//Proceso
 	area= (perimetro*apotema)/2;
-	
+	//Salida
 	cout<< area;
-	
 	return 0;
 }
