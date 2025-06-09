4db4ecc269209727bd02339d44364667ecb8992a
ShirleyCauna <caunashirley11@gmail.com>
2025-06-09 16:14:22 -0500

    Agrega validacion de entradas

--- Ejercicio5_secuenciales.cpp
+++ Ejercicio5_secuenciales.cpp
@@ -5,15 +5,32 @@ int main(){
 	int SB,V1,V2,V3,DC,TD;
 	cout<<"Ingrese Sueldo base: ";
 	cin>> SB;
-	cout<<"Ingrese Venta 1: ";
-	cin>> V1;
-	cout<<"Ingrese Venta 2: ";
-	cin>> V2;
-	cout<<"Ingrese Venta 3: ";
-	cin>> V3;
-	DC=(V1+V2+V3)*0.01;
-	TD=SB+DC;
-	cout<<"Dinero de las comisiones a un 10%: " << DC <<endl;
-	cout<<"Total del dinero a recibir: " << TD << endl;
+	if (SB>0){
+		cout<<"Ingrese Venta 1: ";
+	    cin>> V1;
+	    if (V1>0){
+	    	cout<<"Ingrese Venta 2: ";
+	        cin>> V2;
+	        if (V2>0){
+	        	cout<<"Ingrese Venta 3: ";
+	            cin>> V3;
+	            if (V3>0){
+	            	DC=(V1+V2+V3)*0.01;
+	                TD=SB+DC;
+	                cout<<"Dinero de las comisiones a un 10%: " << DC <<endl;
+	                cout<<"Total del dinero a recibir: " << TD << endl;
+				} else {
+					cout <<"Datos no validos";
+				}
+			} else {
+				cout <<"Datos no validos";
+			}
+		} else {
+			cout <<"Datos no validos";
+		}
+	} else {
+		cout <<"Datos no validos";
+	}
+	
 	return 0;
 }
